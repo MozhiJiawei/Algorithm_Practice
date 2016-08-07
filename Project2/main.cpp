cf869ee522db9e5751da3bd71327b657146779c9
L-T440Windows <372745456@qq.com>
2016-08-07 15:10:05 +0800

    All

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,7 +1,7 @@
 /*
 3 3
-...
-...
+X.X
+.X.
 ...
 0 1
 4
@@ -10,6 +10,7 @@
 -1 0
 0 -1
 */
+/*
 #include <iostream>
 #include <vector>
 #include <set>
@@ -21,11 +22,11 @@ struct step {
 
 vector<vector<bool> > room;
 vector<step> steps;
-int n, m, min_step = -1;
+int n, m, min_step = -1, tar_x, tar_y;
 
 void Travel(int x, int y, int cur_step, vector<vector<bool> > past_steps) {
     past_steps[x][y] = false;
-    if (x == n - 1 && y == m - 1) { 
+    if (x == tar_x && y == tar_y) { 
         if (min_step == -1) {
             min_step = cur_step;
         }
@@ -53,6 +54,7 @@ int main() {
     vector<vector<bool> > past_steps;
     int x0, y0;
     int k;
+    int result = -1;
     cin >> n >> m;
     for (int i = 0; i < n; i++) {
         vector<bool> room_line;
@@ -81,7 +83,103 @@ int main() {
         s.dy = y;
         steps.push_back(s);
     } 
-    Travel(x0, y0, 0, past_steps);
-    cout << min_step;
+    for (int i = 0; i < n; i++) { 
+        for (int j = 0; j < m; j++) {
+            if (i == x0 && j == y0) {
+                continue;
+            }
+            if (room[i][j] == true) {
+                tar_x = i;
+                tar_y = j;
+                min_step = -1;
+                Travel(x0, y0, 0, past_steps); 
+                if (result == -1) {
+                    result = min_step;
+                }
+                else if (min_step > result) {
+                    result = min_step;
+                }
+            }
+        }
+    }
+    cout << result << flush;
     return 0; 
+}
+*/
+
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+struct step {
+    step(int xx, int yy):x(xx), y(yy) {}
+    int x;
+    int y;
+};
+
+int main() {
+    vector<vector<int> > room;
+    int x0, y0, m, n, k;
+    vector<step> steps;
+    int max_step = -1;
+    vector<step> cur_point;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        vector<int> room_line;
+        for (int j = 0; j < m; j++) {
+            char input;
+            cin >> input;
+            if (input == '.') {
+                room_line.push_back(-1);
+            }
+            if (input == 'X') {
+                room_line.push_back(-2);
+            }
+        }
+        room.push_back(room_line);
+    }
+    cin >> x0 >> y0; 
+    cin >> k;
+    for (int i = 0; i < k; i++) {
+        int x, y;
+        cin >> x >> y;
+        steps.push_back(step(x, y));
+    }
+    cur_point.push_back(step(x0, y0));
+    room[x0][y0] = 0;
+    while (true) {
+        bool no_other_root = true;
+        vector<step> new_cur_point;
+        for (size_t i = 0; i < cur_point.size(); i++) {
+            for (size_t j = 0; j < steps.size(); j++) { 
+                int nx, ny;
+                nx = cur_point[i].x + steps[j].x;
+                ny = cur_point[i].y + steps[j].y;
+                if (nx < n && nx >= 0 &&
+                    ny < m && ny >= 0) { 
+
+                    if (room[nx][ny] == -1) { 
+                        if (max_step == -1) room[nx][ny] = 1;
+                        else room[nx][ny] = max_step + 1;
+                        no_other_root = false;
+                        new_cur_point.push_back(step(nx, ny));
+                    }
+                }
+            }
+        }
+        cur_point = new_cur_point;
+        if (no_other_root) {
+            break;
+        }
+        else {
+            if (max_step == -1) {
+                max_step = 1;
+            }
+            else {
+                max_step++;
+            }
+        }
+    }
+    cout << max_step << flush;
+    return 0;
 }
